43fb290720b62b66ed0c01353d1b242fbd64bc2d
mridul-iiitn <mridulpandey619@gmail.com>
2025-11-21 19:38:51 +0530

    Add new route for fetching all conversations

--- server/fileroute.js
+++ server/fileroute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { createEmbeddingFromPDF, query, fetchConversationHistory,fetchAllConversationsh,conversationHistory,createConversation, fetchStats } = require("./fileController");
 
+//all routes
 router.get("/pdf", createEmbeddingFromPDF);
 router.get("/stats", fetchStats);
 router.post("/query", query);
